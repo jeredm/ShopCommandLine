b0c65ea372569906b42218efa2bbe944f8ff2da6
Jered Myers <jeredamyers@gmail.com>
2020-04-17 11:06:48 -0700

    Init repo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShopCommandLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Shop Command Line
+
+TODO - Initial README
+
+So far... Ensure you have .NET Core 3.1 installed.
